0a447591d76369a3c1b5be1d8d2f8eca42e27e75
fabioyudi <fabioyudi@NBPP027013.BRQ.COM>
2018-01-22 16:24:03 -0200

    Primeiro

---
+++ Teste/src/test/java/Fabio/Teste/AppTest.java
@@ -0,0 +1,41 @@
+package Fabio.Teste;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+    	Calculadora c = new Calculadora();
+    	c.setValor1(2);
+    	c.setValor2(2);
+        assertEquals("O valor deveria ser 4", 4,  c.calcular());
+    }
+}
